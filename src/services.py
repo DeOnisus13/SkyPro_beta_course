a438e34c4dafbd5eba9c498c1d0d84fa33dba146
Denis Antropov <de94onisus@gmail.com>
2023-12-10 18:16:56 +0400

    Add docstring for src/services.py

--- src/services.py
+++ src/services.py
@@ -3,6 +3,12 @@ from typing import Any
 
 
 def investment_bank(transactions: list[dict], year_month: str, limit: int) -> Any:
+    """
+    Функция "Инвестиционная копилка" для вычисления суммы, которую можно получить от округления платежей до заданного
+    порога.
+    На вход подаются список словарей, дата в формате год-месяц и число, до которого нужно округлить платеж.
+    Выход - сумма, которую можно было отложить в инвестиции.
+    """
     date = datetime.strptime(year_month, "%Y-%m")
 
     for transaction in transactions:
